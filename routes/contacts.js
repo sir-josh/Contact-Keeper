1778bf9e53042921f5153a598c9dab73996feb19
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2019-11-21 23:35:32 +0100

    Create Add contact for specific users'

--- routes/contacts.js
+++ routes/contacts.js
@@ -23,8 +23,28 @@ router.get('/', auth, async (req, res)=>{
 // @route   POST    api/contacts
 // @desc    Add new contacts
 // @access  Private
-router.post('/', (req, res)=>{
-    res.send('Add contacts');
+router.post('/',[ auth, [
+    check('name', 'Name is required').not().isEmpty()
+]], 
+async (req, res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+        const newContact = new Contact({ name, email, phone, type, user: req.user.id });
+
+        const contact = await newContact.save();
+
+        res.json(contact);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 // @route   PUt    api/contacts/:id
