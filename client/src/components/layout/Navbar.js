521aa064a5eb1523c738330ce6635d3da7215d8c
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2019-11-22 15:24:52 +0100

    Create Home and About Pages on react client app

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,10 +1,15 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({title, icon})=>{
   return(
       <div className="navbar bg-primary">
         <h3><i className={icon} /> &nbsp; {title}</h3>
+         <ul>
+           <li> <Link to="/">Home</Link> </li>
+           <li> <Link to="/about">About</Link> </li>
+         </ul>
       </div>
   )
 }
